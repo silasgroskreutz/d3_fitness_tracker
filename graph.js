30eae8a7039d27745792b920c12d7378283f6cd5
Silas Groskreutz <50895337+silasgroskreutz@users.noreply.github.com>
2020-03-28 13:04:03 -0500

    initial work on graph.js

---
+++ graph.js
@@ -0,0 +1,29 @@
+const update = data => {
+  console.log(data);
+};
+
+// for data and firestore
+var data = [];
+
+db.collection('activites').onSnapshot(res => {
+  res.docChanges().forEach(change => {
+    const doc = { ...change.doc.data(), id: change.doc.id };
+
+    switch (change.type) {
+      case 'added':
+        data.push(doc);
+        break;
+      case 'modified':
+        const index = data.findIndex(item => item.id == doc.id);
+        data[index] = doc;
+        break;
+      case 'removed':
+        data = data.filter(item => item.id !== doc.id);
+        break;
+      default:
+        break;
+    }
+  });
+
+  update(data);
+});
